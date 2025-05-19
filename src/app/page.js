63faffc31a542a9914b0cb8c7ce1ac90cb4a21db
julou <louis.jungo@gmail.com>
2025-05-19 13:06:27 +0200

    build fix 2

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Gallery from "./components/Gallery";
 export default function Home() {
   return (
     <>
-      <div className="min-w-screen min-h-screen overflow-y-hidden bg-black flex justify-center items-center">
+      <div className="min-w-100 xxl:min-w-max min-h-screen overflow-y-hidden overflow-x-hidden bg-black flex justify-center items-center">
         <div className="absolute container text-8xl lg:text-[150px] text-shadow-lg/60 font-(family-name:--font-anton-sc) p-9">Lord Lawrence & The Lard Guitar</div>
         <video className="min-h-screen object-cover" autoPlay muted loop>
           <source src="/money-promo.mp4" type="video/mp4"></source>
@@ -14,7 +14,7 @@ export default function Home() {
         
       </div>
       <Header />
-      <div className="min-w-screen min-h-120 bg-slate-50 text-slate-900">
+      <div className="min-w-100 min-h-120 bg-slate-50 text-slate-900">
         <div className="container flex flex-col lg:flex-row mx-auto p-4 pt-15">
             <div className="lg:mr-10 basis-1/2">
               <Image src={'/group.jpg'} width={0} height={0} sizes="100vw" className="mb-10 w-[100%] h-auto rounded-lg" alt="group picture" />
@@ -24,19 +24,19 @@ export default function Home() {
               Since their formation in 2023, the four enthusiastic musicians have been transporting audiences into a world in which the D-standard guitars and hard drum beats invite you to headbang along. The band feels at home in stoner rock, although other musical genres can also be heard in the songs. The gripping melodies are guaranteed to reverberate in your ears even after the concert. Look forward to an exciting, varied concert that you won&apos;t forget in a hurry.</span>
         </div>
       </div>
-      <div className="min-w-screen min-h-120">
+      <div className="min-w-100 min-h-120">
         <div className="container mx-auto p-4 py-15">
           <h1 className="text-center">Music</h1>
           <iframe src="https://open.spotify.com/embed/artist/1k4FnGJS0FNsIGuN6uZE6Q?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
       </div>
-      <div className="min-w-screen min-h-120 bg-slate-50 text-slate-900">
+      <div className="min-w-100 min-h-120 bg-slate-50 text-slate-900">
         <div className="container mx-auto p-4 py-15">
           <h1 className="text-center">Gallery</h1>
           <Gallery />
         </div>
       </div>
-      <div className="min-w-screen min-h-120">
+      <div className="min-w-100 min-h-120">
         <div className="container mx-auto p-4 pt-15">
           <h1 className="text-center">Contact</h1>
         </div>
