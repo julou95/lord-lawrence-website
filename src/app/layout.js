d8de9ec3dc262f6b1c1d69a61524f29ffa6f04a2
julou <louis.jungo@gmail.com>
2025-10-07 20:22:59 +0200

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono, Anton_SC } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
         className={`${geistMono.variable} ${antonSC.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
